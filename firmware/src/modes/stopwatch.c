48b2e1f8b38f7b298f303adc32a2e80e945e2548
Rex McKinnon <rex@mckinnon.ninja>
2023-02-03 17:16:27 -0800

    Added countdown mode

--- firmware/src/modes/stopwatch.c
+++ firmware/src/modes/stopwatch.c
@@ -19,13 +19,24 @@
 
 #include "modes/stopwatch.h"
 
+enum stopwatch_types {
+    TIMER_STOPWATCH,
+    TIMER_COUNTDOWN,
+    TIMER_MAX_TIMERS
+};
+
+static unsigned char stopwatch_type = 0;
+
 static unsigned char stopwatch_active = 0;
 
-static long hexnumber = 0;
+void timer_stopwatch_run (unsigned int event);
+void timer_countdown_run (unsigned int event);
+
 
 void
 stopwatch_init (void)
 {
+    stopwatch_type = TIMER_STOPWATCH;
     stopwatch_active = 0;
 }
 
@@ -35,23 +46,63 @@ stopwatch_start (void)
     // Secondary display tag
     display_secondary_character(1, 'S');
     display_secondary_segments(2,0b0010110001);
+    // the letter 'T', but we use additional segments
 
     // Display stopwatch time
     // display_primary_string(1, "00 00 00");
     // display_period(DISPLAY_PERIOD_COLON);
-    tick_rate_set_ms(500);
-    hexnumber = 0;
-    display_primary_hex(-1, hexnumber);
 }
+
 
 signed char
 stopwatch_run (unsigned int event)
+{
+    // We catch all button presses and respond to them the same way:
+    // * MODE - A press of the mode button behaves like it does all the time,
+    //      by cycling to the next mode by returning 1.
+    // * ADJ - A press of the adj button will cycle to the next type of timer.
+    //      Currently, it just switches between stopwatch and countdown.
+    if (EVENT_TYPE(event) == EVENT_BUTTON)
+    {
+        if (EVENT_DATA(event) == BUTTON_MODE_PRESS)
+        {
+            // Return 1 to signal to switch modes
+            return 1;
+        }
+        else if (EVENT_DATA(event) == BUTTON_ADJ_PRESS)
+        {
+            // Adj button cycles to the next timer
+            stopwatch_type = (stopwatch_type + 1) % TIMER_MAX_TIMERS;
+            return 0;
+        }
+    }
+
+    switch (stopwatch_type)
+    {
+    case TIMER_STOPWATCH:
+        timer_stopwatch_run(event);
+    break;
+
+    case TIMER_COUNTDOWN:
+        timer_countdown_run(event);
+    break;
+    
+    default:
+        // Cycle to next mode if we somehow get a timer type we don't recognize.
+        return 1;
+    break;
+    }
+
+    return 0;
+}
+
+void
+timer_stopwatch_run (unsigned int event)
 {
     switch (EVENT_TYPE(event))
     {
 
     case EVENT_TICK:
-        display_primary_hex(-1, hexnumber++);
         if (stopwatch_active)
         {
             // display_primary
@@ -74,22 +125,17 @@ stopwatch_run (unsigned int event)
     break;
 
 
-    case EVENT_BUTTON:
-        if (EVENT_DATA(event) == BUTTON_MODE_PRESS)
-        {
-            // Return 1 to signal to switch modes
-            return 1;
-        }
-        else if (EVENT_DATA(event) == BUTTON_ADJ_PRESS)
-        {
-        }
+
     break;
     
     default:
     break;
     }
+}
 
-    return 0;
+void
+timer_countdown_run (unsigned int event)
+{
 }
 
 void

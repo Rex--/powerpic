9b67539413aeb7661da2d8c2de07d4986bb979c6
Rex McKinnon <rex@mckinnon.ninja>
2024-02-08 18:41:39 -0800

    timer mode: fixed bugs in countdown timer, added mario chime

--- firmware/src/modes/timer.c
+++ firmware/src/modes/timer.c
@@ -14,6 +14,7 @@
 #include "lib/buzzer.h"
 #include "lib/backlight.h"
 #include "lib/datetime.h"
+#include "lib/alarm.h"
 
 #define LOG_TAG "mode.timer"
 #include "lib/logging.h"
@@ -55,7 +56,7 @@ static unsigned char timer_type = 0;
 //// Variables for countdown timer ////
 
 // Any number above 0 signifies that the timer is active
-static unsigned char timer_countdown_active = 0;
+static volatile unsigned char timer_countdown_active = 0;
 
 // Any number above 0 signifies that we are editing the timer
 static unsigned char timer_countdown_edit = 0;
@@ -67,7 +68,7 @@ static unsigned char timer_countdown_repeat = 0;
 static datetime_t timer_countdown_alarm = {0,0,0,0,0,0,0};
 
 // This is the time of the current countdown timer
-static time_t timer_countdown_time = {0,0,0};
+static volatile time_t timer_countdown_time = {0,0,0};
 
 // This is the time that the countdown timer was originally set to.
 static time_t timer_countdown_reset = {0,0,0};
@@ -77,18 +78,20 @@ static time_t timer_countdown_reset = {0,0,0};
 //// Helper functions ////
 void timer_next (void);
 static void timer_display_time (time_t *time);
+static unsigned char timer_add_time (time_t *timeA, time_t *timeB);
+static unsigned char timer_sub_time (time_t *timeA, time_t *timeB);
 
 
 void
 timer_init (void)
 {
-    timer_type = TIMER_STOPWATCH;
+    timer_type = TIMER_COUNTDOWN;
 
     timer_countdown_active = 0;
     timer_countdown_edit = 0;
     timer_countdown_repeat = 0;
-    timer_countdown_reset.minute = 15; // Countdown timer defaults to 15 minutes
-    timer_countdown_time.minute = 15;  // with no time having elapsed
+    timer_countdown_reset.second = 15; // Countdown timer defaults to 15 minutes
+    timer_countdown_time.second = 15;  // with no time having elapsed
 }
 
 void
@@ -130,9 +133,6 @@ timer_countdown_start (void)
     display_primary_string(1, "00 00 00");
     display_period(DISPLAY_PERIOD_COLON);
 
-    // Then display actual timer on top of that (keeps the zeros)
-    timer_display_time(&timer_countdown_time);
-
     // Display multiplication sign if timer is set to repeat
     if (timer_countdown_repeat)
     {
@@ -143,7 +143,26 @@ timer_countdown_start (void)
     {
         // If the timer is active set tick rate to 1 second
         tick_rate_set_sec(1);
+        // Get the new timer duration (time may have passed since last time mode was active)
+        // Populate time object with alarm time
+        timer_countdown_time = timer_countdown_alarm.time;
+        // Store current time in temp variable
+        time_t current_time;
+        datetime_time_now(&current_time);
+
+        // Subtract current time from timer alarm time
+        if (timer_sub_time(&timer_countdown_time, &current_time))
+        {
+            // If the function returns a 1, it means timer has elapsed
+            // We set it to 0 here, and the code in our run function will catch it
+            timer_countdown_time.second = 0;
+            timer_countdown_time.minute = 0;
+            timer_countdown_time.hour = 0;
+        }
     }
+
+    // Display actual timer on top of zeros
+    timer_display_time(&timer_countdown_time);
 }
 
 
@@ -254,21 +273,21 @@ timer_countdown_run (unsigned int event)
                     }
                     else // Timer is finished
                     {
-                        if (timer_countdown_repeat)
-                        {
-                            // Repeat timer
-                            timer_countdown_time = timer_countdown_reset;
-                            timer_display_time(&timer_countdown_time);
-                        }
-                        else
-                        {
-                            // Do not repeat timer, disable ticks and set timer inactive
-                            tick_disable();
-                            timer_countdown_active = 0;
-                        }
+                        // Set timer to 0
+                        timer_countdown_time.second = 0;
+                        timer_countdown_time.minute = 0;
+                        timer_countdown_time.hour = 0;
+                        tick_disable();
                     }
                 }
             }
+        }
+        else
+        {
+            // Disable ticks if timer is inactive
+            tick_disable();
+            // Display time
+            timer_display_time(&timer_countdown_time);
         }
     break;
 
@@ -280,6 +299,7 @@ timer_countdown_run (unsigned int event)
                 {
                     timer_countdown_active = 0; // Deactivate countdown
                     tick_disable(); // Disable ticks
+                    alarm_del_event(TIMER_COUNTDOWN_ALARM_EVENT); // Delete the registered alarm
                 }
 
                 else
@@ -293,6 +313,20 @@ timer_countdown_run (unsigned int event)
                     }
                     timer_countdown_active = 1; // Enable countdown
                     tick_rate_set_sec(1);  // Enable ticks
+
+                    // Calculate absolute time that timer should end
+                    // Populate datetime object with current time
+                    datetime_now(&timer_countdown_alarm);
+                    // Add the timer value to current time
+                    if (timer_add_time(&timer_countdown_alarm.time, &timer_countdown_time))
+                    {
+                        // Add function returns a 1 if day rolls over. We don't
+                        // handle months rolling over so this may not work at
+                        // the end of the month.
+                        timer_countdown_alarm.date.day = (unsigned char)DEC2BCD(BCD2DEC(timer_countdown_alarm.date.day) + 1);
+                    }
+                    // Register an alarm at the time the timer ends.
+                    alarm_set_datetime(&timer_countdown_alarm, TIMER_COUNTDOWN_ALARM_EVENT);
                 }
             break;
 
@@ -360,7 +394,6 @@ timer_countdown_run (unsigned int event)
                     timer_countdown_reset.minute = (unsigned char)((timer_countdown_reset.minute << 4) | (timer_countdown_reset.second >> 4));
                     timer_countdown_reset.second = (unsigned char)((timer_countdown_reset.second << 4) | (new_number));
                     // Update display with new time
-
                     timer_display_time(&timer_countdown_reset);
                 }
             break;
@@ -381,6 +414,57 @@ timer_stop (void)
     display_sign_clear(DISPLAY_SIGN_MULTIPLY);
 }
 
+
+
+void
+timerd (unsigned int event)
+{
+    if (EVENT_TYPE(event) == EVENT_ALARM)
+    {
+        switch (EVENT_DATA(event))
+        {
+            case TIMER_COUNTDOWN_ALARM_EVENT:
+                timer_countdown_time = timer_countdown_reset;
+                if (timer_countdown_repeat)
+                {
+                    // Repeat timer
+                    // timer_display_time(&timer_countdown_time);
+
+                    // Calculate absolute time that timer should end
+                    // Populate datetime object with current time
+                    datetime_now(&timer_countdown_alarm);
+                    // Add the timer value to current time
+                    if (timer_add_time(&timer_countdown_alarm.time, &timer_countdown_time))
+                    {
+                        // Add function returns a 1 if day rolls over. We don't
+                        // handle months rolling over so this may not work at
+                        // the end of the month.
+                        timer_countdown_alarm.date.day = (unsigned char)DEC2BCD(BCD2DEC(timer_countdown_alarm.date.day) + 1);
+                    }
+                    // Register an alarm at the time the timer ends.
+                    alarm_set_datetime(&timer_countdown_alarm, TIMER_COUNTDOWN_ALARM_EVENT);
+                }
+                else
+                {
+                    // Do not repeat timer, disable ticks and set timer inactive
+                    // tick_disable();
+                    timer_countdown_active = 0;
+                    // Clear timer seconds in case it was behind
+                    timer_countdown_time.second = 0;
+                }
+                LOG_DEBUG("Countdown ended");
+                // Play mario song
+                static char song[] = "Mario:d=4,o=5,b=200:32p,16e6,16e6,16p,16e6,16p,16c6,16e6,16p,16g6,8p,16p,16g,8p,32p,16c6,8p,16g,8p,16e,8p,16a,16p,16b,16p,16a#,16a,16p,16g,16e6,16g6,16a6,16p,16f6,16g6,16p,16e6,16p,16c6,16d6,16b,p,16g6,16f#6,16f6,16d#6,16p,16e6,16p,16g#,16a,16c6,16p,16a,16c6,16d6,8p,8d#6,16p,16d6,8p,8c6";
+                buzzer_play_rtttl(song);
+            break;
+
+            default:
+            break;
+        }
+    }
+}
+
+
 void
 timer_next (void)
 {
@@ -400,6 +484,69 @@ timer_display_time (time_t *time)
     display_primary_number(2, BCD2DEC(time->hour));
     display_primary_number(5, BCD2DEC(time->minute));
     display_primary_number(8, BCD2DEC(time->second));
+}
+
+// This helper function adds timeB to timeA.
+// Returns a 1 if day rolls over, 0 otherwise
+static unsigned char
+timer_add_time (time_t *timeA, time_t *timeB)
+{
+    // Add seconds
+    timeA->second = (unsigned char)DEC2BCD(BCD2DEC(timeB->second) + BCD2DEC(timeA->second));
+    if (0x59 < timeA->second) {
+        // Roll over and add one to minute
+        timeA->second = (unsigned char)DEC2BCD(BCD2DEC(timeA->second) % 60);
+        timeA->minute = (unsigned char)DEC2BCD(BCD2DEC(timeA->minute) + 1);
+    }
+    // Add minutes
+    timeA->minute = (unsigned char)DEC2BCD(BCD2DEC(timeA->minute) + BCD2DEC(timeB->minute));
+    if (0x59 < timeA->minute) {
+        // Roll over and add one minute to hour
+        timeA->minute = (unsigned char)DEC2BCD(BCD2DEC(timeA->minute) % 60);
+        timeA->hour = (unsigned char)DEC2BCD(BCD2DEC(timeA->hour) + 1);
+    }
+    // Add hours
+    timeA->hour = (unsigned char)DEC2BCD(BCD2DEC(timeA->hour) + BCD2DEC(timeB->hour));
+    if (0x23 < timeA->hour) {
+        // Roll over hour and return a 1
+        timeA->hour = (unsigned char)DEC2BCD(BCD2DEC(timeA->hour) % 24);
+        return 1;
+    }
+    return 0;
+}
+
+// This helper function subtracts timeB from timeA.
+// Returns a 1 if day rolls over, 0 otherwise
+static unsigned char
+timer_sub_time (time_t *timeA, time_t *timeB)
+{
+    unsigned char ret = 0;
+    
+    // Subtract seconds
+    if (timeB->second > timeA->second) {
+        // Add 60 seconds and subtract one minute
+        timeA->second = (unsigned char)DEC2BCD(BCD2DEC(timeA->second) + 60);
+        timeA->minute = (unsigned char)DEC2BCD(BCD2DEC(timeA->minute) - 1);
+    }
+    timeA->second = (unsigned char)DEC2BCD(BCD2DEC(timeA->second) - BCD2DEC(timeB->second));
+    
+    // Subtract minutes
+    if (timeB->minute > timeA->minute) {
+        // Add 60 minutes and subtract one hour
+        timeA->minute = (unsigned char)DEC2BCD(BCD2DEC(timeA->minute) + 60);
+        timeA->hour = (unsigned char)DEC2BCD(BCD2DEC(timeA->hour) - 1);
+    }
+    timeA->minute = (unsigned char)DEC2BCD(BCD2DEC(timeA->minute) - BCD2DEC(timeB->minute));
+    
+    // Subtract hours
+    if (timeB->hour > timeA->hour) {
+        // Add 24 hours and return a 1
+        timeA->hour = (unsigned char)DEC2BCD(BCD2DEC(timeA->hour) + 24);
+        ret = 1;
+    }
+    timeA->hour = (unsigned char)DEC2BCD(BCD2DEC(timeA->hour) - BCD2DEC(timeB->hour));
+
+    return ret;
 }
 
 // EOF //
--- firmware/src/modes/timer.h
+++ firmware/src/modes/timer.h
@@ -6,10 +6,13 @@
 #ifndef _timer_h_
 #define _timer_h_
 
+#define TIMER_COUNTDOWN_ALARM_EVENT 0xCD
+
 void            timer_init  (void);
 void            timer_start (void);
 signed char     timer_run   (unsigned int event);
 void            timer_stop  (void);
+void            timerd (unsigned int event);
 
 mode_app_t timer_mode = {
     "timer",
@@ -17,6 +20,7 @@ mode_app_t timer_mode = {
     &timer_start,
     &timer_run,
     &timer_stop,
+    &timerd
 };
 
 #endif
